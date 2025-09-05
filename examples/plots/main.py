5eb2f75c5a284c421d634433f86186716dc1fa4a
kahleeeb3 <powellcalebm@gmail.com>
2025-09-05 03:13:45 -0500

    added a plotting example

---
+++ examples/plots/main.py
@@ -0,0 +1,12 @@
+from webserver_helper import WebServer, VideoChannel
+from RandomScatterPlots import *
+
+server = WebServer()
+plot = RandomScatterPlot()
+server_plot = VideoChannel(server.app, "/plot")
+server.start()
+
+while True:
+    plot.update()
+    plot_frame = plot.get_frame_data(quality=20)
+    server_plot.update(plot_frame)
